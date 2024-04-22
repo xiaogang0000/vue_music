1c0bf9ecdcc73f872dc3acfd739a55ea2df8c492
xiaogang <505992537.com>
2024-04-22 12:07:09 +0800

    4.22第二次

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <m-header></m-header>
  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  MHeader from './components/m-header/m-header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   MHeader
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 import "~common/stylus/varible"
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
